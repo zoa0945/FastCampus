37b2ed12e36fd49ebee7872f50e9eceb151ed80f
Mac <zoa0945@gmail.com>
2021-12-30 17:42:44 +0900

    complete

---
+++ Timer/RoundButton.swift
@@ -0,0 +1,19 @@
+//
+//  RoundButton.swift
+//  Timer
+//
+//  Created by Mac on 2021/12/23.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+}
