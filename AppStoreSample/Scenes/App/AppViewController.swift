1187d96db2e95c21cc2844dabfd30c02c79356d3
Mac <zoa0945@gmail.com>
2022-04-04 23:51:37 +0900

    feat: add FeatureSectionView

--- AppStoreSample/Scenes/App/AppViewController.swift
+++ AppStoreSample/Scenes/App/AppViewController.swift
@@ -9,10 +9,34 @@ import UIKit
 import SnapKit
 
 class AppViewController: UIViewController {
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        
+        let featureSectionView = FeatureSectionView(frame: .zero)
+        let rankingFeatureSectionView = UIView()
+        let exchangeCodeButtonView = UIView()
+        
+        rankingFeatureSectionView.backgroundColor = .blue
+        exchangeCodeButtonView.backgroundColor = .yellow
+        
+        [featureSectionView, rankingFeatureSectionView, exchangeCodeButtonView].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationController()
+        setupLayout()
     }
 }
 
@@ -21,5 +45,24 @@ extension AppViewController {
         navigationItem.title = "앱"
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
