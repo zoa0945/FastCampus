a42d10b0f9be4b9d666833897854680e793fd305
Mac <zoa0945@gmail.com>
2022-04-12 23:32:46 +0900

    feat: make SearchController

---
+++ SubwayInfo/StationSearchViewController.swift
@@ -0,0 +1,74 @@
+//
+//  StationSearchViewController.swift
+//  SubwayInfo
+//
+//  Created by Mac on 2022/04/12.
+//
+
+import UIKit
+import SnapKit
+
+class StationSearchViewController: UIViewController {
+    private var numberOfCell = 0
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.isHidden = true
+        
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    private func setNavigationItems() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "지하철 도착 정보"
+        
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "지하철 역명을 입력해주세요."
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        
+        navigationItem.searchController = searchController
+    }
+    
+    private func setTableViewLayout() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension StationSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfCell
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath)"
+        
+        return cell
+    }
+}
+
+
+extension StationSearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 10
+        tableView.isHidden = false
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 0
+        tableView.isHidden = true
+    }
+}
