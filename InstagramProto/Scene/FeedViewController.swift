ee20e57f35e5b173d6365f633049cc8fbf2aa66c
Mac <zoa0945@gmail.com>
2022-04-18 22:34:19 +0900

    feat: make FeedViewController UI

---
+++ InstagramProto/Scene/FeedViewController.swift
@@ -0,0 +1,64 @@
+//
+//  FeedViewController.swift
+//  InstagramProto
+//
+//  Created by Mac on 2022/04/18.
+//
+
+import UIKit
+import SnapKit
+
+class FeedViewController: UIViewController {
+    private lazy var feedTableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        
+        tableView.register(FeedTableViewCell.self, forCellReuseIdentifier: "FeedTableViewCell")
+        
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        setupTableView()
+    }
+}
+
+extension FeedViewController {
+    func setupNavigationBar() {
+        navigationItem.title = "Instagram"
+        let uploadButton = UIBarButtonItem(
+            image: UIImage(systemName: "plus.app"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        navigationItem.rightBarButtonItem = uploadButton
+    }
+    func setupTableView() {
+        view.addSubview(feedTableView)
+        feedTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none
+        cell.setup()
+        
+        return cell
+    }
+}
---
+++ InstagramProto/Scene/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  InstagramProto
+//
+//  Created by Mac on 2022/04/18.
+//
+
+import UIKit
+import SnapKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let feedViewController = UINavigationController(rootViewController: FeedViewController())
+        feedViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        let profileViewController = UIViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
+        
+        viewControllers = [feedViewController, profileViewController]
+    }
+}
