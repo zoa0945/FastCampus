6bc873d33d1ebd1b28deddde12e9704bb0804976
zoa0945 <zoa0945@gmail.com>
2022-12-14 23:38:14 +0900

    chore: 16. add Rxswift comment

--- 16.GithubRepoList/GithubRepoList/ListViewController.swift
+++ 16.GithubRepoList/GithubRepoList/ListViewController.swift
@@ -11,6 +11,8 @@ import RxCocoa
 
 class ListViewController: UITableViewController {
     private let organization = "ReactiveX"
+    
+    // 네트워크 통신이 끝난 뒤 결과같이 들어갈 빈 Sequence
     private let repositories = BehaviorSubject<[Repository]>(value: [])
     private let disposeBag = DisposeBag()
 
@@ -36,22 +38,28 @@ class ListViewController: UITableViewController {
         }
     }
     
+    // 네트워크 통신을 통해 데이터를 불러온 뒤 Subject에 onNext 이벤트를 발생시키는 함수
     func fetchRepositories(of organization: String) {
         Observable.from([organization])
+            // String to URL
             .map { organization -> URL in
                 return URL(string: "https://api.github.com/orgs/\(organization)/repos")!
             }
+            // URL to URLRequest
             .map { url -> URLRequest in
                 var request = URLRequest(url: url)
                 request.httpMethod = "GET"
                 return request
             }
+            // URLRequest to Observable
             .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+                // UIKit에서 사용되던 URLSession을 rx로 변환해줌
                 return URLSession.shared.rx.response(request: request)
             }
             .filter { response, _ in
                 return 200..<300 ~= response.statusCode
             }
+            // Data to [String: Any] : Data형태를 JSON 형태의 Dictionary로 Decoding
             .map { _, data -> [[String: Any]] in
                 guard let json = try? JSONSerialization.jsonObject(with: data, options: []),
                       let result = json as? [[String: Any]] else {
@@ -62,6 +70,7 @@ class ListViewController: UITableViewController {
             .filter { result in
                 result.count > 0
             }
+            // JSON to Repository : 변환된 Dictionary형태를 Repository로 변환
             .map { objects in
                 return objects.compactMap { dic -> Repository? in
                     guard let id = dic["id"] as? Int,
