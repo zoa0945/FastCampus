3abf0759fc00d71aad910200245ab95f574bca49
zoa0945 <zoa0945@gmail.com>
2022-12-15 20:23:13 +0900

    17. add comment of CombiningOperator

--- 17.SearchBlog/CombiningOperator.playground/Contents.swift
+++ 17.SearchBlog/CombiningOperator.playground/Contents.swift
@@ -3,6 +3,9 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
+// 초기값(시작값)을 onNext 이벤트 전에 추가
+// 해당 Observable과 같은 타입을 추가할 수 있음
+// 여러개의 sequence를 순서대로 append하는 방식
 print("-----startWith-----")
 let yellow = Observable<String>.of("first", "second", "third")
 
@@ -13,6 +16,8 @@ yellow
     })
     .disposed(by: disposeBag)
 
+// sequence와 sequence를 합쳐줌
+// 여러개의 sequence를 순서대로 append하는 방식
 print("-----concat-----")
 let yellow2 = Observable<String>.of("first", "second", "third")
 let teacher = Observable<String>.of("teacher")
@@ -33,6 +38,8 @@ teacher
     })
     .disposed(by: disposeBag)
 
+// 여러개의 sequence 중 매핑되는 sequence를 순서대로 나타냄
+// 여러개의 sequence를 순서대로 append하는 방식
 print("-----concatMap-----")
 let student: [String: Observable<String>] = [
     "yellow": Observable.of("first", "second", "third"),
@@ -48,6 +55,9 @@ Observable.of("yellow", "blue")
     })
     .disposed(by: disposeBag)
 
+// 순서를 보장하지 않고 여러개의 sequence를 합침
+// merge되는 여러개의 sequence는 독립적으로 하나의 sequence에 에러가 발생하면
+// merge되는 Observable 또한 error 이벤트를 방출하고 종료됨
 print("-----merge-----")
 let south = Observable.of("강남", "서초", "강동", "송파")
 let north = Observable.of("강북", "동대문", "성동", "은평")
@@ -59,6 +69,8 @@ Observable.of(south, north)
     })
     .disposed(by: disposeBag)
 
+// maxConcurrnet: 순서를 보장할 sequence의 개수를 정의
+// maxConcurrent 만큼의 sequence가 방출되면 순서 상관없이 방출됨
 print("-----merge2-----")
 Observable.of(south, north)
     .merge(maxConcurrent: 1)
@@ -67,6 +79,8 @@ Observable.of(south, north)
     })
     .disposed(by: disposeBag)
 
+// combineLatest에 의해 결합된 여러개의 sequence들은 값이 방출될 때마다
+// closure를 호출하게 되고 각 sequence에서 가장 최근에 방출된 값들을 사용하여 closure 내부 작업을 진행
 print("-----combineLatest-----")
 let firstName = PublishSubject<String>()
 let secondName = PublishSubject<String>()
@@ -124,6 +138,9 @@ secondName.onNext("Paul")
 secondName.onNext("Stella")
 secondName.onNext("Lily")
 
+// zip된 여러개의 sequence의 같은 index에 있는 element를 방출해
+// closure 내부 작업을 진행
+// element가 모두 방출된 sequence가 하나라도 있으면 중단
 print("-----zip-----")
 enum WinLose {
     case win
@@ -144,6 +161,8 @@ result
     })
     .disposed(by: disposeBag)
 
+// 트리거가 되는 Observable에 onNext 이벤트가 발생하게 되면
+// withLatestFrom에 들어간 두번째 Observable의 가장 최근 이벤트가 실행됨
 print("-----withLatestFrom-----")
 let trigger = PublishSubject<Void>()
 let runner = PublishSubject<String>()
@@ -163,6 +182,8 @@ runner.onNext("third")
 trigger.onNext(Void())
 trigger.onNext(Void())
 
+// withLatestFrom과 같은 동작을 하지만
+// 트리거가 되는 Observable이 단 한번의 이벤트만 발생시킨다
 print("-----sample-----")
 let start = PublishSubject<Void>()
 let f1 = PublishSubject<String>()
@@ -180,6 +201,9 @@ f1.onNext("third")
 start.onNext(Void())
 start.onNext(Void())
 
+// amb로 연결된 두개의 Obeervable을 모두 구독하지만
+// 두개 중 이벤트가 먼저 발생한 Observable의 이벤트만 방출하고
+// 나머지 Observable의 이벤트는 발생시키지 않음
 print("-----amb-----")
 let bus1 = PublishSubject<String>()
 let bus2 = PublishSubject<String>()
@@ -202,6 +226,7 @@ bus2.onNext("customer4")
 bus1.onNext("customer7")
 bus1.onNext("customer8")
 
+// source Observable로 들어온 마지막 sequence의 이벤트만 방출하게 됨
 print("-----switchLatest-----")
 let student1 = PublishSubject<String>()
 let student2 = PublishSubject<String>()
@@ -236,6 +261,19 @@ student2.onNext("hi9")
 student3.onNext("hi10")
 student2.onNext("hi11")
 
+// swift의 고차함수 reduce와 같은 동작
+print("-----reduce-----")
+Observable.from((1...10))
+    .reduce(0) { summary, newValue in
+        return summary + newValue
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+// reduce와 같이 동작하지만 scan은 매번 element가 들어올 때마다
+// 변하는 값들을 방출하여 이벤트를 발생시킴
 print("-----scan-----")
 Observable.from((1...10))
     .scan(0, accumulator: +)
