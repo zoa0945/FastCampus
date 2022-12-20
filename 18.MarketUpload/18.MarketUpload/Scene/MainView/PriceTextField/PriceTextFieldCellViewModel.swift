0e6ae75011bc439321f74a90dcd89d1af5b4073c
zoa0945 <zoa0945@gmail.com>
2022-12-20 22:37:19 +0900

    18. add pricetextfield comment

--- 18.MarketUpload/18.MarketUpload/Scene/MainView/PriceTextField/PriceTextFieldCellViewModel.swift
+++ 18.MarketUpload/18.MarketUpload/Scene/MainView/PriceTextField/PriceTextFieldCellViewModel.swift
@@ -10,10 +10,16 @@ import RxSwift
 import RxCocoa
 
 class PriceTextFieldCellViewModel {
+    // ViewModel에서 View로 전달되는 데이터
+    // 가격을 작성하지 않으면 무료나눔을 보여주기 위한 Signal
     let showFreeShareButton: Signal<Bool>
+    // 제출 버튼을 눌렀을 때 가격이 초기화 되기위한 Signal
     let resetPrice: Signal<Void>
     
+    // View에서 ViewModel로 전달되는 데이터
+    // 입력한 가격의 데이터
     let priceValue = PublishRelay<String?>()
+    // 무료나눔 tap 이벤트
     let freeShareButtonTapped = PublishRelay<Void>()
     
     init() {
