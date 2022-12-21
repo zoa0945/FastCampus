11afe8fcb22e8b10edc7f86c82e293a19d61742d
zoa0945 <zoa0945@gmail.com>
2022-12-21 22:21:11 +0900

    19. add KAKAO Map SDK and set frameworks

---
+++ 19.FIndCVS/19.FIndCVS/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  19.FIndCVS
+//
+//  Created by zoa0945 on 2022/12/21.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* BridgingHeader_h */
