eeb6d1aed352d10e5f9d9546a5cddb384d16e3c2
zoa0945 <zoa0945@gmail.com>
2022-12-27 20:08:44 +0900

    19. add error description

--- 19.FIndCVS/19.FIndCVS/MTMapViewError.swift
+++ 19.FIndCVS/19.FIndCVS/MTMapViewError.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+enum MTMapViewError: Error {
+    case failedUpdationCurrentLocation
+    case locationAuthorizationDenied
+    
+    var errorDescription: String {
+        switch self {
+        case .failedUpdationCurrentLocation:
+            return "현재 위치를 불러오지 못했어요. 잠시 후 다시 시도해주세요."
+        case .locationAuthorizationDenied:
+            return "위치 정보를 비활성화하면 사용자의 현재 위치를 알 수 없어요."
+        }
+    }
+}
